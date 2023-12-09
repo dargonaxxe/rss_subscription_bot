5a7fd92eb0ced26c6e8f43d1e71fb7952e8a69e5
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-09 19:14:38 +0400

    subscription changeset + unit tests

--- lib/rss_subscription_bot/core/subscription/subscription.ex
+++ lib/rss_subscription_bot/core/subscription/subscription.ex
@@ -1,10 +1,24 @@
 defmodule RssSubscriptionBot.Core.Subscription do
   use Ecto.Schema
+  alias RssSubscriptionBot.Core.Subscription
   alias RssSubscriptionBot.Core.User
 
   schema "subscriptions" do
     field(:url, :string)
     belongs_to(:user, User)
     timestamps()
+  end
+
+  def new() do
+    %Subscription{}
+  end
+
+  import Ecto.Changeset
+
+  def changeset(%Subscription{} = subscription, attrs \\ %{}) do
+    subscription
+    |> cast(attrs, [:url, :user_id])
+    |> validate_required([:url, :user_id])
+    |> assoc_constraint(:user)
   end
 end
--- test/rss_subscription_bot/core/subscription/subscription_test.exs
+++ test/rss_subscription_bot/core/subscription/subscription_test.exs
@@ -2,6 +2,10 @@ defmodule RssSubscriptionBot.Core.SubscriptionTest do
   alias RssSubscriptionBot.Core.Subscription
   use RssSubscriptionBot.SchemaCase
 
+  defp create_subscription(_) do
+    %{subscription: %Subscription{}}
+  end
+
   describe "fields & types" do
     @expected_fields_and_types [
       id: :id,
@@ -13,6 +17,36 @@ defmodule RssSubscriptionBot.Core.SubscriptionTest do
 
     test "should have expected fields and types" do
       assert_has_fields(Subscription, @expected_fields_and_types)
+    end
+  end
+
+  describe "new" do
+    setup [:create_subscription]
+
+    test "should return expected result", %{subscription: sub} do
+      assert sub == Subscription.new()
+    end
+  end
+
+  describe "changeset" do
+    @user_id 1
+    @url "url"
+    @attrs_no_url %{user_id: @user_id}
+    @attrs_no_user_id %{url: @url}
+    @attrs_valid %{user_id: @user_id, url: @url}
+
+    test "should validate url presence" do
+      %{errors: [url: {_, [validation: :required]}]} =
+        Subscription.new() |> Subscription.changeset(@attrs_no_url)
+    end
+
+    test "should validate user_id presence" do
+      %{errors: [user_id: {_, [validation: :required]}]} =
+        Subscription.new() |> Subscription.changeset(@attrs_no_user_id)
+    end
+
+    test "should return valid changeset" do
+      %{valid?: true} = Subscription.new() |> Subscription.changeset(@attrs_valid)
     end
   end
 end
