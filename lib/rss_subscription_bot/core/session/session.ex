60dc04bd7aaffdf1849f5f27982c16242a1416c5
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-09 13:48:05 +0400

    session sign in changeset + unit test

--- lib/rss_subscription_bot/core/session/session.ex
+++ lib/rss_subscription_bot/core/session/session.ex
@@ -2,10 +2,26 @@ defmodule RssSubscriptionBot.Core.Session do
   alias RssSubscriptionBot.Core.Account
   use Ecto.Schema
 
+  alias RssSubscriptionBot.Core.Session
+
   schema "sessions" do
     belongs_to(:account, Account)
     field(:token, :binary)
     field(:valid_until, :naive_datetime)
     timestamps()
+  end
+
+  def new() do
+    %Session{}
+  end
+
+  import Ecto.Changeset
+
+  def sign_in_changeset(%Session{} = session, attrs \\ %{}) do
+    session
+    |> cast(attrs, [:account_id, :token, :valid_until])
+    |> validate_required([:account_id, :token, :valid_until])
+    |> assoc_constraint(:account)
+    |> check_constraint(:valid_until, name: :sessions_valid_until_constraint)
   end
 end
--- test/rss_subscription_bot/core/session/session_test.exs
+++ test/rss_subscription_bot/core/session/session_test.exs
@@ -3,6 +3,10 @@ defmodule RssSubscriptionBot.Core.SessionTest do
   alias RssSubscriptionBot.SchemaCase
   use SchemaCase
 
+  defp create_session(_) do
+    %{session: %Session{}}
+  end
+
   describe "fields & types" do
     @expected_fields_and_types [
       account_id: :id,
@@ -15,6 +19,44 @@ defmodule RssSubscriptionBot.Core.SessionTest do
 
     test "should have expected fields and types" do
       assert_has_fields(Session, @expected_fields_and_types)
+    end
+  end
+
+  describe "new()" do
+    setup [:create_session]
+
+    test "should return expected result", %{session: session} do
+      assert Session.new() == session
+    end
+  end
+
+  describe "sign_in_changeset" do
+    setup [:create_session]
+    @account_id 1
+    @token "12345678123456781234567812345678"
+    @valid_until ~N"2024-01-01 23:00:00"
+    @attrs_no_account_id %{token: @token, valid_until: @valid_until}
+    @attrs_no_token %{account_id: @account_id, valid_until: @valid_until}
+    @attrs_no_valid_until %{account_id: @account_id, token: @token}
+    @attrs_valid %{account_id: @account_id, token: @token, valid_until: @valid_until}
+
+    test "should validate account_id presence", %{session: session} do
+      %{errors: [account_id: {_, [validation: :required]}]} =
+        session |> Session.sign_in_changeset(@attrs_no_account_id)
+    end
+
+    test "should validate token presence", %{session: session} do
+      %{errors: [token: {_, [validation: :required]}]} =
+        session |> Session.sign_in_changeset(@attrs_no_token)
+    end
+
+    test "should validate valid_until presence", %{session: session} do
+      %{errors: [valid_until: {_, [validation: :required]}]} =
+        session |> Session.sign_in_changeset(@attrs_no_valid_until)
+    end
+
+    test "should return no errors", %{session: session} do
+      %{valid?: true} = session |> Session.sign_in_changeset(@attrs_valid)
     end
   end
 end
