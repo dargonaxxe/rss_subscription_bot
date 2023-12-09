d3b682777f0f55b696043e93e177f6347cdde0e6
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-09 19:41:53 +0400

    user changeset

--- lib/rss_subscription_bot/core/user/user.ex
+++ lib/rss_subscription_bot/core/user/user.ex
@@ -1,8 +1,22 @@
 defmodule RssSubscriptionBot.Core.User do
+  alias RssSubscriptionBot.Core.User
   use Ecto.Schema
 
   schema "users" do
     belongs_to(:account, RssSubscriptionBot.Core.Account)
     timestamps()
+  end
+
+  def new() do
+    %User{}
+  end
+
+  import Ecto.Changeset
+
+  def changeset(%User{} = user, attrs \\ %{}) do
+    user
+    |> cast(attrs, [:account_id])
+    |> validate_required([:account_id])
+    |> assoc_constraint(:account)
   end
 end
--- test/rss_subscription_bot/core/user/user_test.exs
+++ test/rss_subscription_bot/core/user/user_test.exs
@@ -3,6 +3,10 @@ defmodule RssSubscriptionBot.Core.UserTest do
 
   alias RssSubscriptionBot.Core.User
 
+  defp create_user(_) do
+    %{user: %User{}}
+  end
+
   describe "fields & types" do
     @expected_fields_and_types [
       account_id: :id,
@@ -13,6 +17,29 @@ defmodule RssSubscriptionBot.Core.UserTest do
 
     test "should have expected fields and types" do
       assert_has_fields(User, @expected_fields_and_types)
+    end
+  end
+
+  describe "new" do
+    setup [:create_user]
+
+    test "should return expected result", %{user: user} do
+      assert user == User.new()
+    end
+  end
+
+  describe "changeset" do
+    @account_id 1
+    @attrs_invalid %{}
+    @attrs_valid %{account_id: @account_id}
+
+    test "should validate account_id presence" do
+      %{errors: [account_id: {_, [validation: :required]}]} =
+        User.new() |> User.changeset(@attrs_invalid)
+    end
+
+    test "should return valid changeset" do
+      %{valid?: true} = User.new() |> User.changeset(@attrs_valid)
     end
   end
 end
