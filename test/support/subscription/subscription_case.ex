01d6c14e079ad5f721ffa16f695413932a95dd07
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-10 17:56:01 +0400

    subscription case

---
+++ test/support/subscription/subscription_case.ex
@@ -0,0 +1,17 @@
+defmodule RssSubscriptionBot.SubscriptionCase do
+  alias RssSubscriptionBot.Core.Subscriptions
+  alias RssSubscriptionBot.UserCase
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      use UserCase
+      setup [:setup_user]
+
+      defp setup_subscription(%{user: %{id: user_id}}) do
+        {:ok, subscription} = Subscriptions.create_subscription(user_id, "url", "tg_handle")
+        %{subscription: subscription}
+      end
+    end
+  end
+end
