129871d6947dfcf74021ddd5f7b289466460970b
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-12 17:29:16 +0400

    user observer ping message

--- lib/rss_subscription_bot/rss/otp/user_observer.ex
+++ lib/rss_subscription_bot/rss/otp/user_observer.ex
@@ -1,4 +1,5 @@
 defmodule RssSubscriptionBot.Rss.Otp.UserObserver do
+  alias RssSubscriptionBot.Rss.Otp.SubscriptionObserver
   alias RssSubscriptionBot.Core.Subscriptions
   use GenServer
 
@@ -17,12 +18,32 @@ defmodule RssSubscriptionBot.Rss.Otp.UserObserver do
   @impl GenServer
   def handle_continue(:get_subscriptions, %{user_id: user_id} = state) do
     subscriptions = Subscriptions.get_subscriptions(user_id)
-    {:noreply, put_in(state.subscriptions, subscriptions)}
+
+    {:noreply, put_in(state.subscriptions, subscriptions),
+     {:continue, :wake_subscription_observers}}
+  end
+
+  @impl GenServer
+  def handle_continue(:wake_subscription_observers, state) do
+    state.subscriptions
+    |> Enum.each(fn x ->
+      {:ok, _} =
+        {:via, Registry,
+         {RssSubscriptionBot.Registry, {__MODULE__.DynamicSupervisor, state.user_id}}}
+        |> DynamicSupervisor.start_child({SubscriptionObserver, x})
+    end)
+
+    {:noreply, state}
   end
 
   @impl GenServer
   def handle_cast(:ping, state) do
-    IO.puts("pong")
+    state.subscriptions
+    |> Enum.each(fn x ->
+      {:via, Registry, {RssSubscriptionBot.Registry, {SubscriptionObserver, x.id}}}
+      |> GenServer.cast(:ping)
+    end)
+
     {:noreply, state}
   end
 end
