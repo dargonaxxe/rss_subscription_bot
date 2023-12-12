d7b238669a302744a6f2984cadece0510bab7c28
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-12 17:12:25 +0400

    subscription observer state initialization

---
+++ lib/rss_subscription_bot/rss/otp/subscription_observer.ex
@@ -0,0 +1,32 @@
+defmodule RssSubscriptionBot.Rss.Otp.SubscriptionObserver do
+  alias RssSubscriptionBot.Core.Feed.Item
+  alias RssSubscriptionBot.Core.Feed
+  use GenServer
+
+  defstruct [:subscription, :fetched_items, :last_fetched_datetime]
+
+  def start_link(subscription) do
+    name = {:via, Registry, {RssSubscriptionBot.Registry, {__MODULE__, subscription.id}}}
+    GenServer.start_link(__MODULE__, subscription, name: name)
+  end
+
+  @impl GenServer
+  def init(subscription) do
+    {:ok, %__MODULE__{subscription: subscription}, {:continue, :get_items}}
+  end
+
+  @impl GenServer
+  def handle_continue(:get_items, %{subscription: %{id: sub_id}} = state) do
+    items =
+      Feed.get_items(sub_id)
+      |> Enum.map(&Item.to_domain/1)
+
+    {:noreply, put_in(state.fetched_items, items)}
+  end
+
+  @impl GenServer
+  def handle_cast(:ping, state) do
+    IO.puts("pong")
+    {:noreply, state}
+  end
+end
