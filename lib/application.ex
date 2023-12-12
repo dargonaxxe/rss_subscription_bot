1de457c28ea4f0006160faa1101bd54ac3e89671
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-12 13:10:54 +0400

    added name registry

--- lib/application.ex
+++ lib/application.ex
@@ -2,7 +2,7 @@ defmodule RssSubscriptionBot.Application do
   use Application
 
   def start(_, _) do
-    children = [finch(), repo()]
+    children = [finch(), repo(), registry()]
 
     opts = [strategy: :one_for_one, name: RssSubscriptionBot.Supervisor]
     Supervisor.start_link(children, opts)
@@ -22,5 +22,9 @@ defmodule RssSubscriptionBot.Application do
 
   defp repo() do
     RssSubscriptionBot.Repo
+  end
+
+  defp registry() do
+    {Registry, keys: :unique, name: RssSubscriptionBot.Registry}
   end
 end
