5f7429cfb6d63b03bdf852e27f82cee82b7d8485
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-13 18:52:13 +0400

    pubsub config

--- apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/application.ex
+++ apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/application.ex
@@ -2,17 +2,16 @@ defmodule RssSubscriptionBotWeb.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
+  alias Phoenix.PubSub
 
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
-      RssSubscriptionBotWeb.Telemetry,
-      # Start a worker by calling: RssSubscriptionBotWeb.Worker.start_link(arg)
-      # {RssSubscriptionBotWeb.Worker, arg},
-      # Start to serve requests, typically the last entry
-      RssSubscriptionBotWeb.Endpoint
+      telemetry(),
+      endpoint(),
+      pub_sub()
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -27,5 +26,17 @@ defmodule RssSubscriptionBotWeb.Application do
   def config_change(changed, _new, removed) do
     RssSubscriptionBotWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp telemetry() do
+    RssSubscriptionBotWeb.Telemetry
+  end
+
+  defp endpoint() do
+    RssSubscriptionBotWeb.Endpoint
+  end
+
+  defp pub_sub() do
+    {PubSub, name: RssSubscriptionBotWeb.PubSub}
   end
 end
