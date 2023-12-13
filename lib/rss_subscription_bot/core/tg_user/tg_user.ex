97986f8d05503b27cdaa47478520b018fe83fa6f
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-13 15:41:31 +0400

    tg_user changeset

--- lib/rss_subscription_bot/core/tg_user/tg_user.ex
+++ lib/rss_subscription_bot/core/tg_user/tg_user.ex
@@ -6,4 +6,16 @@ defmodule RssSubscriptionBot.Core.TgUser do
     field(:handle, :string)
     timestamps()
   end
+
+  def new() do
+    %__MODULE__{}
+  end
+
+  import Ecto.Changeset
+
+  def changeset(%__MODULE__{} = tg_user, attrs \\ %{}) do
+    tg_user
+    |> cast(attrs, [:tg_id, :handle])
+    |> validate_required([:tg_id, :handle])
+  end
 end
--- test/rss_subscription_bot/core/tg_user/tg_user_test.exs
+++ test/rss_subscription_bot/core/tg_user/tg_user_test.exs
@@ -16,4 +16,32 @@ defmodule RssSubscriptionBot.Core.TgUserTest do
       assert_has_fields(TgUser, @expected_fields_and_types)
     end
   end
+
+  describe "new" do
+    test "should return expected result" do
+      assert TgUser.new() == %TgUser{}
+    end
+  end
+
+  describe "changeset" do
+    @tg_id 1
+    @handle "handle"
+    @attrs_no_tg_id %{handle: @handle}
+    @attrs_no_handle %{tg_id: @tg_id}
+    @attrs_valid %{tg_id: @tg_id, handle: @handle}
+
+    test "should validate tg_id presence" do
+      %{errors: [tg_id: {_, [validation: :required]}]} =
+        TgUser.new() |> TgUser.changeset(@attrs_no_tg_id)
+    end
+
+    test "should validate handle presence" do
+      %{errors: [handle: {_, [validation: :required]}]} =
+        TgUser.new() |> TgUser.changeset(@attrs_no_handle)
+    end
+
+    test "should return valid changeset" do
+      %{valid?: true} = TgUser.new() |> TgUser.changeset(@attrs_valid)
+    end
+  end
 end
