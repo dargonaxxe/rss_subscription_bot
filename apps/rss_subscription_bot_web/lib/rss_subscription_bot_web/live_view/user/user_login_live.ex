c6fec84e99c3f643c5fd66dcd63d35b9b1408bef
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-14 14:16:04 +0400

    login liveview render & mount methods

--- apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/live_view/user/user_login_live.ex
+++ apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/live_view/user/user_login_live.ex
@@ -2,12 +2,21 @@ defmodule RssSubscriptionBotWeb.UserLoginLive do
   use RssSubscriptionBotWeb, :live_view
 
   def mount(_, _, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(form: %{} |> to_form(as: :user))}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Hello there!</h1>
+    <.simple_form for={@form} phx-update="ignore" action={~p"/user/login"}>
+      <.input field={@form[:username]} label="Username" required/>
+      <.input field={@form[:pwd_string]} label="Password" type="password" required/>
+      <:actions> 
+        <.button phx-disable-with="Logging in...">
+          Login
+        </.button>
+      </:actions>
+    </.simple_form>
     """
   end
 end
