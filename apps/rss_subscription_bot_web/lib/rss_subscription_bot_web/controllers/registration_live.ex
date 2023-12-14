6efb70e2abcacb7140fda32580593ae777b2065a
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-14 11:13:06 +0400

    registration live view mount method

--- apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/controllers/registration_live.ex
+++ apps/rss_subscription_bot_web/lib/rss_subscription_bot_web/controllers/registration_live.ex
@@ -1,8 +1,24 @@
 defmodule RssSubscriptionBotWeb.RegistrationLive do
+  alias Ecto.Changeset
+  alias RssSubscriptionBot.Core.Account
   use RssSubscriptionBotWeb, :live_view
 
   def mount(_, _, socket) do
+    socket = socket |> assign_account() |> assign_changeset()
     {:ok, socket}
+  end
+
+  def assign_account(socket) do
+    socket |> assign(account: Account.new())
+  end
+
+  def assign_changeset(%{assigns: %{account: account}} = socket) do
+    changeset = account |> Account.registration_changeset(%{})
+    socket |> assign_form(changeset)
+  end
+
+  def assign_form(socket, %Changeset{} = changeset) do
+    socket |> assign(form: changeset |> to_form())
   end
 
   def render(assigns) do
