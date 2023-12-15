2d2a980674e455e5f9f36e0abb21326d2f881ff4
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-15 15:46:48 +0400

    url validation for create subscription form

--- apps/rss_subscription_bot/lib/rss_subscription_bot/core/subscription/subscription.ex
+++ apps/rss_subscription_bot/lib/rss_subscription_bot/core/subscription/subscription.ex
@@ -21,6 +21,25 @@ defmodule RssSubscriptionBot.Core.Subscription do
     subscription
     |> cast(attrs, [:url, :user_id, :tg_handle, :name])
     |> validate_required([:url, :user_id, :tg_handle, :name])
+    |> validate_url()
     |> assoc_constraint(:user)
+  end
+
+  defp validate_url(%{changes: changes} = changeset) do
+    url = changes |> Map.get(:url)
+
+    if is_valid?(url) do
+      changeset
+    else
+      changeset |> add_error(:url, "Invalid url")
+    end
+  end
+
+  defp is_valid?(nil), do: false
+
+  # suggested url validation from stackoverflow 
+  defp is_valid?(url) do
+    uri = url |> URI.parse()
+    uri.scheme != nil && uri.host =~ "."
   end
 end
